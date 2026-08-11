8ea0abec94c4cdd491bd141da2942c6e032ea92f
Aditya Chaudhary <chadityan2110@gmail.com>
2026-08-11 07:39:32 +0530

    prepare backend for production deployment

--- server/src/constants/cookie.constants.js
+++ server/src/constants/cookie.constants.js
@@ -1,13 +1,13 @@
 export const accessTokenOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
 export const refreshTokenOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
-  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  maxAge: 30 * 24 * 60 * 60 * 1000,
 };
